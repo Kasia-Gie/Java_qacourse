a2332928d7b952ee97ab28c517feecddd92c174b
Kasia-Gie <k.gorochowik@gmail.com>
2019-10-06 22:12:28 +0200

    Homework No 2

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,13 +3,14 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-    Square s = new Square(5);
-    System.out.println("Pole kwadratu o boku " + s.l + " = " + s.area());
+    Point p1 = new Point(3, 2);
+    Point p2 = new Point(7, 5);
+    System.out.println("Distance between p1 (" +p1.x + ","+p1.y+") and p2 ("+p2.x+","+p2.y+") is " + distance(p1, p2));
+    System.out.println("Distance between p1 (" +p1.x + ","+p1.y+") and p2 ("+p2.x+","+p2.y+") is " + p1.distance(p1,p2));
+  }
 
-    Rectangle r = new Rectangle(4, 6);
-
-    System.out.println("Pole prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
   }
 
 }
