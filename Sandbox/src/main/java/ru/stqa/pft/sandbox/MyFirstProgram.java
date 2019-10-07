fbffaf76abcc6e3d76f098fcaa9040ea1df76da0
Kasia-Gie <k.gorochowik@gmail.com>
2019-10-07 09:34:22 +0200

    finished homework 2

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,15 +2,15 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    Point p1 = new Point(3, 2);
-    Point p2 = new Point(7, 5);
-    System.out.println("Distance between p1 (" +p1.x + ","+p1.y+") and p2 ("+p2.x+","+p2.y+") is " + distance(p1, p2));
-    System.out.println("Distance between p1 (" +p1.x + ","+p1.y+") and p2 ("+p2.x+","+p2.y+") is " + p1.distance(p1,p2));
-  }
+    public static void main(String[] args) {
+        Point p1 = new Point(3, 2);
+        Point p2 = new Point(7, 5);
+        System.out.println("Distance between p1 (" + p1.x + "," + p1.y + ") and p2 (" + p2.x + "," + p2.y + ") is " + distance(p1, p2));
+        System.out.println("Distance between p1 (" + p1.x + "," + p1.y + ") and p2 (" + p2.x + "," + p2.y + ") is " + p1.distance(p2));
+    }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
 
 }
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,14 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double x;
-  public double y;
+    public double x;
+    public double y;
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
- public double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point secondPoint) {
+        return Math.sqrt(Math.pow((secondPoint.x - this.x), 2) + Math.pow((secondPoint.y - this.y), 2));
+    }
 }
