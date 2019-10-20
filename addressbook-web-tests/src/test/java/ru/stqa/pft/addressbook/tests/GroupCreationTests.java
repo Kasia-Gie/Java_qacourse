534b4d9432da2e2ee641d3f1aeeb5b8705bb5465
Gorochowik Katarzyna <katarzynago@nask.ad>
2019-10-20 13:52:21 +0200

    Going to different pages optimalization

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -12,6 +12,7 @@ public class GroupCreationTests extends TestBase {
         app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
+        app.getNavigationHelper().goToHomePage();
         app.logout();
     }
 
