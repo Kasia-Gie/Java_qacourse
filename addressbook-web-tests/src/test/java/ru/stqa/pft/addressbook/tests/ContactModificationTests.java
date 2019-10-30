76b62db5dd2b3675faf72f0ed6af2729891de136
Gorochowik Katarzyna <katarzynago@nask.ad>
2019-10-30 22:09:58 +0100

    Homework No. 10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -15,19 +15,20 @@ public class ContactModificationTests extends TestBase {
             app.getNavigationHelper().promptGoToHomePage();
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().selectContact( before.size()- 1);
+        app.getContactHelper().selectContact(before.size() - 1);
         app.getContactHelper().editContact();
-        ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"test1", "test2", "123456789", "test@test.com", null);
+        ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "test1", "test2", "123456789", "test@test.com", null);
         app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getNavigationHelper().promptGoToHomePage();
-
-
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size());
 
         before.remove(before.size() - 1);
         before.add(contact);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
